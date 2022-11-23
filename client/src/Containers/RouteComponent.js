aa7d0135471884a0241031e0b11d5e0d8c8d5b0e
Varsha Reddy Kumbham <kumbham.v@northeastern.edu>
2022-11-23 01:16:13 -0500

    Created side nav and header

--- client/src/Containers/RouteComponent.js
+++ client/src/Containers/RouteComponent.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from './Login/Login.js';
 import SignUp from './SignUp/SignUp.js';
 import SideNav from '../components/SideNav/SideNav.js';
-import Header from '../components/Header/Header';
 // import ForgotPassword from './ForgotPassword/ForgotPassword.js';
 import {ProtectedRoute} from './ProtectedRoute.js';
 //import Dashboard from './Dashboard/Dashboard.js';
