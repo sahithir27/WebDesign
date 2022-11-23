fd261d29b8c9915c7b792045890e08a78a2a180f
Varsha Reddy Kumbham <kumbham.v@northeastern.edu>
2022-11-23 01:14:56 -0500

    Created side nav and header

--- client/src/Containers/ProtectedRoute.js
+++ client/src/Containers/ProtectedRoute.js
@@ -6,5 +6,6 @@ export const ProtectedRoute = () => {
 
     // If authorized, return an outlet that will render child elements
     // If not, return element that will navigate to login page
-    return auth ? <Outlet /> : <Navigate to="/login" />;
+    //return auth ? <Outlet /> : <Navigate to="/login" />;
+    return <Outlet />
 }
--- client/src/Containers/RouteComponent.js
+++ client/src/Containers/RouteComponent.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from './Login/Login.js';
 import SignUp from './SignUp/SignUp.js';
-import Header from '../components/Header/Header.js';
+import SideNav from '../components/SideNav/SideNav.js';
+import Header from '../components/Header/Header';
 // import ForgotPassword from './ForgotPassword/ForgotPassword.js';
 import {ProtectedRoute} from './ProtectedRoute.js';
 //import Dashboard from './Dashboard/Dashboard.js';
@@ -10,13 +11,14 @@ const RoutesComponent = (props) => {
     return (
       <Router>
         <Routes>
-          <Route path="/login" element = {<Login/>} />
-          <Route element = {<Header/>}>
+          {/* <Route element = {<><Header/><SideNav/></>}> */}
+          <Route element = {<SideNav/>}>
         <Route path="/" element = {<ProtectedRoute/>}>
           {/* <Route path="/" element = {<Dashboard/> }/> */}
         </Route> 
         </Route>
           <Route path="/signup" element = {<SignUp/>} />
+          <Route path="/login" element = {<Login/>} />
           {/* <Route path="/forgot-password" element = {<ForgotPassword/>} /> */}
         </Routes>
     </Router>
