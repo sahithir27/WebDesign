2b9a11d47edf58966f53b599df443034669da6f2
Varsha Reddy Kumbham <kumbham.v@northeastern.edu>
2022-11-24 17:46:11 -0500

    Added Calendar

--- client/src/Containers/RouteComponent.js
+++ client/src/Containers/RouteComponent.js
@@ -5,16 +5,26 @@ import ForgotPassword from './ForgotPassword/ForgotPassword.js';
 import SideNav from '../components/SideNav/SideNav.js';
 import {ProtectedRoute} from './ProtectedRoute.js';
 import UserProfile from './UserProfile/UserProfile.js';
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 const RoutesComponent = (props) => {
+  const calendarID = process.env.REACT_APP_CALENDAR_ID;
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+  const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN;
     return (
       <Router>
         <Routes>
           <Route element = {<SideNav/>}>
             <Route path="/profile" element = {<ProtectedRoute/>}>
               <Route path="/profile" element = {<UserProfile/> }/>
-            </Route> 
-            
+            </Route>
+            <Route path="/calendar" element = {<ProtectedRoute/>}>
+              <Route path="/calendar" element = {<FullCalendar
+              plugins={[ dayGridPlugin ]}
+              initialView="dayGridMonth"
+              /> }/>
+            </Route>  
         </Route>
          <Route path="/login" element = {<Login/>} />
           <Route path="/signup" element = {<SignUp/>} />
