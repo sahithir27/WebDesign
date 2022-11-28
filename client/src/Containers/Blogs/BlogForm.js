ded51e12f8d281e04e087619e18b45a7c050f0fa
Varsha Reddy Kumbham <kumbham.v@northeastern.edu>
2022-11-27 22:34:08 -0500

    Added img rul input for blog

--- client/src/Containers/Blogs/BlogForm.js
+++ client/src/Containers/Blogs/BlogForm.js
@@ -13,59 +13,22 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 export class BlogFormComponent extends Component {
     constructor(props) {
         super(props)
-        this.state = { showForm: false,
-            imgUrl: ''
+        this.state = { 
+            showForm: false
         }
-
       }
-      fileSelectedHandler = (e) =>{
-        
-        this.setState({
-            selectedImage: e.target.files[0]
-        }, async ()=> {
-            try {
-                let imageUrl = "";
-                if (this.state.selectedImage) {
-                    const formData = new FormData();
-                    formData.append("file", this.state.selectedImage);
-                    formData.append("upload_preset", "Flexx-Appeal");
-                    let config = {
-                        onUploadProgress : ProgressEvent => {
-                            let percentageCompleted = Math.floor((ProgressEvent.loaded * 100) / ProgressEvent.total);
-                            this.setState({
-                                imageUploadStatus: percentageCompleted
-                            })
-
-                            if(percentageCompleted === 100){
-                                this.setState({
-                                    imageUploadStatus: ""
-                                }) 
-                            }
-                            
-                        }
-                    }
-                    const response = await axios.post("https://ask.sqlservercentral.com/themes/base/admin/img/default-coverImage.png", formData, config);
-                    imageUrl = response.data.url;
-                }
-                this.setState({
-                    profileImageUrl: imageUrl
-                });
-            } catch (err) {
-                console.log('error while uploading image to cloudinary');
-            }
-        })
-        
-    }
 
     addNewBlog() {
         const title = document.getElementById('title').value;
         const description = document.getElementById('description').value;
         const author = document.getElementById('author').value;
+        const imgUrl = document.getElementById('imgUrl').value;
         const payload = {
           blogTitle: title,
           description: description,
           author: author,
-          imgUrl: "http://event360.com/wp-content/uploads/2012/01/iStock-858790856-e1537308976623.jpg"
+          //imgUrl: "http://event360.com/wp-content/uploads/2012/01/iStock-858790856-e1537308976623.jpg"
+          imgUrl: imgUrl
         }
         console.log(JSON.stringify(payload))
         this.props.add("http://localhost:9002/blogsData/", payload)
@@ -83,13 +46,10 @@ export class BlogFormComponent extends Component {
           <li>
             <input type="text" id="author" name="blog_author" placeholder='Author'/>
           </li>
+          <li>
+            <input type="url" className="imgUrl" id="imgUrl" name="blog_image" placeholder='Image Url'/>
+          </li>
         </ul>
-        {/* <div className="image-holder">
-            <div className="Mini-loader">
-                <MiniLoader height={230} width={305} />
-            </div> :  <img src={this.props.imgUrl}></img>
-            <input type="file" accept="image/*" onChange={this.fileSelectedHandler.bind(this)} ref={fileInput => this.fileInput = fileInput}></input>
-        </div> */}
         <button className='post-button' onClick={this.addNewBlog.bind(this)}>Post</button>
       </form>
     )
